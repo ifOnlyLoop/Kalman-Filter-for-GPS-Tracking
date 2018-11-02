24dd178b5704e7882e27c617218291c42acce912
Shamekh Al-Suwi <39354280+ifOnlyLoop@users.noreply.github.com>
2018-11-02 15:46:36 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -12,21 +12,34 @@ using namespace std;
 
 int main()
 {
-  int n;
+  int T,n=0;
   double t0,GPSguess,vguess,gpsVar,vVar,
          t,x,y,vx,vy,ax,ay;
   
   freopen("in.txt", "r", stdin);
   freopen("out.txt", "w", stdout);
  
-  cin>>t0>>x0>>y0>>v0>>gpsVar>>vVar>>n;
-  Kalman why(t0,GPSguess,vguess,gpsVar,vVar);
-  t=0;
   
-  while(n--){
-  cin>>x>>y>>v>>TH;
-  why.handleUpdate(t,x,y,vx,vy,ax,ay);
-  }
+  
+  
+cin>>T;
+Kalman why;//(0,0,0,0,0);  
+while(T--){ // test cases
+  
+  cin>>t0>>GPSguess>>vguess>>gpsVar>>vVar>>n;
+  why.intialize(t0,GPSguess,vguess,gpsVar,vVar);
+  cout<<"GPS vareinace of "<<gpsVar<<"\n\n\n";
+  
+    while(n--){ // for the sample
+      
+      cin>>t>>x>>y>>vx>>vy>>ax>>ay;
+      why.handleUpdate(t,x,y,vx,vy,ax,ay);
+      
+    }
+
+}
+
+  
 
   fclose (stdin);  // stop reading data from in.txt
   fclose (stdout); // stop writing data from out.txt
